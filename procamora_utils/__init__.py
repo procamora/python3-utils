9fddee98901a0cf3254d0d549bc66ebbaba63ec6
procamora <pablojoserocamora@gmail.com>
2020-09-15 16:21:41 +0200

    update from Makefile

--- procamora_utils/__init__.py
+++ procamora_utils/__init__.py
@@ -5,3 +5,4 @@ from procamora_utils.interface_sqlite import *
 from procamora_utils.ip import IP
 from procamora_utils.logger import get_logging
 from procamora_utils.ping import ping
+from procamora_utils.client_ssh import create_arg_parser, ClientSSH
